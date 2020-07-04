48770b0c028c5f59e3879bb80382619ffddef6b4
Joe-08 <joe_andre0710@hotmail.com>
2020-07-04 14:01:23 -0500

    feat(RngOMP): Create Rng with OMP

---
+++ RngStream/ranOMP.cpp
@@ -0,0 +1,30 @@
+#include <omp.h>
+#include "RngStream.h"
+#include <iostream>
+
+int main()
+{
+  int nP = omp_get_num_procs();
+  omp_set_num_threads(nP);
+
+  unsigned long seed[6] = { 1806547166, 3311292359,
+                            643431772, 1162448557,
+                            3335719306, 4161054083};
+  
+  RngStream::SetPackageSeed(seed);
+  RngStream RngArray[nP]; //array  of RngStream  objects
+  int myRank;
+
+  #pragma omp parallel private(myRank)
+    {
+      myRank  = omp_get_thread_num();
+      #pragma omp critical
+        {
+          std::cout << RngArray[myRank].RandU01() << " ";
+        }
+    }
+  
+  std::cout << std::endl;
+  return 0;
+}
+
