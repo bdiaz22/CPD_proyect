6c8bb7282122d3023b4b28560c179042131eafe5
Nicolás <nicolas.chipana@utec.edu.pe>
2020-07-11 22:27:12 -0500

    Fixed path directory include

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -1,9 +1,10 @@
-#include "./Material/RngStream.h"
+#include "RngStream/RngStream.h"
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
-#define N 10000
-#define ITER 10
+#include <omp.h>
+#define N 100000
+#define ITER 100
 #define PI 3.1459265358979323846
 using namespace std;
 
@@ -11,37 +12,44 @@ double func(double value){
   return sin(value);
 }
 
-
 int main(){
 
   double upperLimit = PI;
   double lowerLimit = 0.0;
-  double total = 0.0;
   double answer;
   double randomValues[N];
 
-  unsigned long seed[6] = { 170220009, 800813030,  
-			                      221120006, 357035871, 
+  unsigned long seed[6] = { 170220009, 800813030,
+			                      221120006, 357035871,
 			                      953570445, 434738743};
   RngStream::SetPackageSeed(seed);
   RngStream myRngStream;
 
+  omp_set_num_threads(4);
+
+  #pragma omp parallel shared(answer) private(randomValues)
+  {
+  #pragma omp for
   for(int iteration = 0; iteration < ITER; iteration++){
-    
-    total = 0.0; 
+
+    double total = 0.0;
 
     for(int i = 0; i < N; i++){
-      randomValues[i] = fmod(myRngStream.RandU01()*10,upperLimit);
-    }  
+      randomValues[i] = fmod(myRngStream.RandU01()*100,upperLimit);
+    }
 
     for(int i = 0; i < N; i++){
       total += func(randomValues[i]);
     }
 
+    #pragma omp atomic
     answer += (upperLimit - lowerLimit)/(double)N * total;
 
   }
-  cout << answer/ITER<<endl;
+  #pragma omp single
+    cout << answer/ITER<<endl;
+  }
+
 
   return 0;
 }
