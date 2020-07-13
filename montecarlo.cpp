8cfd02d528b822475ae71d79a99d9a1401ee491e
bryan <bryan.diaz@utec.edu.pe>
2020-07-12 20:03:23 -0500

    feat(RngStream): Implementing Monte Carlo in OMP

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -1,9 +1,9 @@
-#include "RngStream/RngStream.h"
+#include "RngStream.h"
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
 #include <omp.h>
-#define N 100000
+#define N 1000000
 #define ITER 100
 #define PI 3.1459265358979323846
 using namespace std;
@@ -19,24 +19,26 @@ int main(){
   double answer;
   double randomValues[N];
 
-  unsigned long seed[6] = { 170220009, 800813030,
-			                      221120006, 357035871,
-			                      953570445, 434738743};
+  unsigned long seed[6] = { 170220009, 800813030,  
+                            221120006, 357035871, 
+                            953570445, 434738743};
   RngStream::SetPackageSeed(seed);
   RngStream myRngStream;
 
-  omp_set_num_threads(4);
+  omp_set_num_threads(8);
 
+  double time = omp_get_wtime();
+  
   #pragma omp parallel shared(answer) private(randomValues)
   {
   #pragma omp for
   for(int iteration = 0; iteration < ITER; iteration++){
-
-    double total = 0.0;
+    
+    double total = 0.0; 
 
     for(int i = 0; i < N; i++){
       randomValues[i] = fmod(myRngStream.RandU01()*100,upperLimit);
-    }
+    }  
 
     for(int i = 0; i < N; i++){
       total += func(randomValues[i]);
@@ -46,10 +48,15 @@ int main(){
     answer += (upperLimit - lowerLimit)/(double)N * total;
 
   }
+  
   #pragma omp single
-    cout << answer/ITER<<endl;
+    {
+      cout << "Tiempo: " << omp_get_wtime() - time << " segundos\n";
+      cout << answer/ITER<<"\n";
+    }
   }
 
+  
 
   return 0;
 }
